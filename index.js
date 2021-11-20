28fc9387df28a0b8eb46e461821ebd824df05550
Mrinmoy09 <mrinmoy0908@gmail.com>
2021-11-20 23:48:07 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const { MongoClient } = require('mongodb');
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+app.use(cors());
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gdsos.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+      await client.connect();
+      const database = client.db('doctors_portal')
+      const appointmentCollection = database.collection('appointments')
+      const usersCollection = database.collection('users')
+
+      app.post('/users' , async(req,res)=>{
+        const user = req.body;
+        const result = await usersCollection.insertOne(user)
+        console.log(result);
+        res.json(result)
+        
+      })
+
+      app.get('/users/:email',async(req,res)=>{
+        const email = req.params.email;
+        const query = {email:email}
+        const user = await usersCollection.findOne(query);
+        let isAdmin = false;
+        if(user?.role === 'admin'){
+          isAdmin=true;
+        }
+        res.json({admin:isAdmin});
+      })
+
+      app.put('/users' , async(req,res)=>{
+        const user = req.body;
+        const filter = {email:user.email};
+        const options = { upsert:true }
+        const updateDoc = { $set:user };
+        const result =  await usersCollection.updateOne(filter,updateDoc,options)
+        // console.log(result);
+        res.json(result)
+      })
+
+      app.put('/users/admin' , async(req,res)=>{
+        const user = req.body;
+        console.log('put' , user);
+        const filter = {email: user.email}
+        const updateDoc = {$set:{role:'admin'}};
+        const result = await usersCollection.updateOne(filter,updateDoc)
+        res.json(result);
+      })
+
+      app.get('/appointments' , async (req,res) =>{
+        const email = req.query.email;
+        const date = new Date(req.query.date).toDateString();
+        console.log(date);
+        const query ={email:email , date:date}
+        const cursor = appointmentCollection.find(query)
+        const result = await cursor.toArray()
+        res.json(result)
+      })
+     
+
+      app.post('/appointments' , async (req,res) => {
+        const appointment = req.body;
+        const result = await appointmentCollection.insertOne(appointment)
+        console.log(result);
+        res.json(result);
+      })
+
+     
+      
+    } finally {
+      // Ensures that the client will close when you finish/error
+    }
+  }
+  run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello Doctors!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
